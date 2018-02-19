4b509447846a454a92498d2b789edb38084badee
Theodore P <evil69669@gmail.com>
2018-02-19 20:15:52 +0200

    added button/form functionality

--- Latitude App/Latitudeapp/app/src/main/java/com/example/kill/latitudeapp/MainActivity.java
+++ Latitude App/Latitudeapp/app/src/main/java/com/example/kill/latitudeapp/MainActivity.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -16,11 +20,17 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    Button click;
+    EditText lat;
+    EditText longt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        click = (Button)findViewById(R.id.click);
+        lat   = (EditText)findViewById(R.id.lat);
+        longt   = (EditText)findViewById(R.id.longt);
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.g_map);
@@ -29,10 +39,21 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        double latitude = 0;
+        double longtitude =0;
+        click.setOnClickListener(
+                new View.OnClickListener()
+                {
+                    public void onClick(View view)
+                    {
 
-        //seattle coordinates
-        LatLng seattle = new LatLng(47.6062095, -122.3320708);
-        mMap.addMarker(new MarkerOptions().position(seattle).title("Seattle"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(seattle));
+                        double latitude = Double.parseDouble(lat.getText().toString());
+                        double longtitude = Double.parseDouble(longt.getText().toString());
+                        LatLng mark = new LatLng( latitude, longtitude);
+                        //Log.v("longt", String.valueOf(latitude));
+                        mMap.addMarker(new MarkerOptions().position(mark).title("Seattle"));
+                        mMap.moveCamera(CameraUpdateFactory.newLatLng(mark));
+                    }
+                });
     }
 }
